bbd241a1b4e935e07644c74b9f7c60310a40c7d1
Nate <152440196+vasilijmaskin536@users.noreply.github.com>
2024-02-04 21:05:51 +0530

    Create form method.swift

---
+++ form method.swift
@@ -0,0 +1,6 @@
+         </style>
+                </head>
+                <body>
+                    <h1>PHP Scratch Game</h1>
+                    <form method="post" action="">
+                        <button type="submit" name="moveLeft">Move Left</button>
